35fe35551d4204158eef7bfb8a176a2d9097cc30
Gaurav <prajapatigaurav355@gmail.com>
2022-10-22 15:59:55 +0530

    Icon Added

--- src/main/java/com/example/melophile/HelloApplication.java
+++ src/main/java/com/example/melophile/HelloApplication.java
@@ -7,19 +7,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 707, 431);
+        stage.getIcons().add(new Image("C://Users//Gaurav//IdeaProjects//Melophile//src//main//resources//com//example//melophile//listening.png"));
         stage.setTitle("Melophile Login");
        stage.setResizable(false);
         stage.setScene(scene);
+
+
+
         stage.show();
 
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
